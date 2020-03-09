b5d1a71ec4f8b4ebf19f494f86bff0c353270b44
Nishit Sharma <nishitsharma0@gmail.com>
2020-03-09 20:09:00 +0530

    Add solution for problem `A - Two Regular Polygons` Link: https://codeforces.com/contest/1312/problem/A Tags: geometry, greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1312/submission/72795838

---
+++ codeforces/1312/A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,m;
+		cin>>n>>m;
+		if(n%m==0)
+			cout<<"YES\n";
+		else
+			cout<<"NO\n";
+	}
+	
+
+	return 0;
+}
