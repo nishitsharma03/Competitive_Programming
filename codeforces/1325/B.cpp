ef50dc91af949fb5e416a8346d827cddcb551e94
Nishit Sharma <nishitsharma0@gmail.com>
2020-03-16 04:13:00 +0530

    Add solution for problem `B - CopyCopyCopyCopyCopy` Link: https://codeforces.com/contest/1325/problem/B Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1325/submission/73371797

---
+++ codeforces/1325/B.cpp
@@ -0,0 +1,39 @@
+#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long 
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll int n;
+		cin>>n;
+		set<ll int> s;
+		fab(0,n,i)
+		{
+			ll int k;
+			cin>>k;
+			s.insert(k);
+		}
+		cout<<s.size()<<endl;
+	}
+	
+
+	return 0;
+}
