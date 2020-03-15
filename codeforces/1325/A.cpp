43e72640ce72d94a3fed1f415dfa68da02305821
Nishit Sharma <nishitsharma0@gmail.com>
2020-03-16 04:06:00 +0530

    Add solution for problem `A - EhAb AnD gCd` Link: https://codeforces.com/contest/1325/problem/A Tags: constructive algorithms, greedy, number theory, *800 Ref: https://codeforces.com/contest/1325/submission/73371644

---
+++ codeforces/1325/A.cpp
@@ -0,0 +1,32 @@
+#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long 
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll int x;
+		cin>>x;
+		cout<<x-1<<" "<<1<<endl;
+	}
+	
+
+	return 0;
+}
