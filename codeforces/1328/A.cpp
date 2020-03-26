0bd23c603a31f82e7667143ea359bf37313d233d
Nishit Sharma <nishitsharma0@gmail.com>
2020-03-26 20:13:00 +0530

    Add solution for problem `A - Divisibility Problem` Link: https://codeforces.com/contest/1328/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1328/submission/74407840

---
+++ codeforces/1328/A.cpp
@@ -0,0 +1,59 @@
+//#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long 
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+vector<ll int > v;
+void printDivisors(int n) 
+{ 
+    // Note that this loop runs till square root 
+    for (int i=1; i<=sqrt(n); i++) 
+    { 
+        if (n%i == 0) 
+        { 
+            // If divisors are equal, print only one 
+            if (n/i == i) 
+                v.pb(i); 
+  
+            else // Otherwise print both 
+                {
+                	v.pb(i);
+                	v.pb(n/i);
+                } 
+        } 
+    } 
+}
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll int a,b;
+		cin>>a>>b;
+		//if()
+		ll int ans=a%b;
+		if(a%b==0)
+			cout<<0<<endl;
+		else
+			
+		cout<<b-ans<<endl;
+
+	}
+	
+
+	return 0;
+}
