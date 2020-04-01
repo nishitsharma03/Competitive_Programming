355d9360c914e5aa8088390c955084d51d3c35fe
Nishit Sharma <nishitsharma0@gmail.com>
2020-04-01 20:31:00 +0530

    Add solution for problem `B - Limericks` Link: https://codeforces.com/contest/1331/problem/B Tags: math, number theory Ref: https://codeforces.com/contest/1331/submission/75119386

---
+++ codeforces/1331/B.cpp
@@ -0,0 +1,35 @@
+//#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long 
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    
+  ll n;
+  cin>>n;
+  fab(2,n+1,i)
+  {
+    if(n%i==0)
+        {cout<<i<<n/i<<endl;
+    return 0;}
+  }
+    
+
+    return 0;
+}
