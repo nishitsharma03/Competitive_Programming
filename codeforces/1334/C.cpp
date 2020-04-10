69513159958af1d6ce2bd8ed75479ae9463b08a8
Nishit Sharma <nishitsharma0@gmail.com>
2020-04-10 21:47:00 +0530

    Add solution for problem `C - Circle of Monsters` Link: https://codeforces.com/contest/1334/problem/C Tags: brute force, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/1334/submission/76177570

--- codeforces/1334/C.cpp
+++ codeforces/1334/C.cpp
@@ -1,66 +1,99 @@
-#include<iostream>
+//#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long 
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
-
 using namespace std;
-long long int mv(long long int a,long long int b)
-{
-    if (a>b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-}
 
 int main()
-{
-    quick;
-    #ifndef ONLINE_JUDGE
+{ quick;
+#ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    #endif
-long long int T;
-cin>>T;
-while(T--)
-{
-    long long int n,fb,fa;
-    cin>>n;
-    long long int A[n][2];
-    for (long long int i=0;i<n;i++)
-    {
-        long long int a,b;
-        cin>>a>>b;
-        A[i][0]=a;
-        A[i][1]=b;
-        if (i==0)
-        {
-            fb=A[0][0];
-        }
-        else
-        {
-            fb=fb+mv(A[i][0]-A[i-1][1],0);
-        }
-    }
-    fa=fb;
-    for (long long int i=1;i<n;i++)
-    {
-        if (i==1)
-        {
-        fb=fb-A[i-1][0]-mv(A[i][0]-A[i-1][1],0)+mv(A[i-1][0]-A[n-1][1],0)+A[i][0];
-        if (fb<fa)
-            fa=fb;
-        }
-        else
-        {
-            fb=fb-A[i-1][0]-mv(A[i][0]-A[i-1][1],0)+mv(A[i-1][0]-A[i-2][1],0)+A[i][0];
-        if (fb<fa)
-            fa=fb;
-        }
-    }
-    cout<<fa<<endl;
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll int n;
+		cin>>n;
+		ll int a[n],b[n];
+		pair<ll int,ll int> p[n];
+		fab(0,n,i)
+		cin>>a[i]>>b[i];
+		//p[i]=mp(a[i],i);}
+		//multimap<ll int ,pair<ll int, ll int>] m;
+		//sort(p,p+n);
+		bool kill[n];
+		//fab(0,n,i)
+		//kill[i]=false;
+		ll int min=a[0],ind=0,sum=0,ans=342355676356477555,diff[n];
+		fab(0,n,i)
+		{
+			if(a[i]<min)
+			{
+				min=a[i];
+				ind=i;
+			}
+			if(a[i]==min)
+			{
+				if(b[i]>b[ind])
+					ind=i;
+			}
+		}
+		fab(0,n,i)
+		{	
+			if(a[(i+1)%n]-b[i]>0)
+			diff[i]=a[(i+1)%n]-b[i];
+		else
+			diff[i]=0;
+			sum+=diff[i];
+		}
+		//cout<<sum<<endl;
+		//fab(0,n,i)
+		//cout<<diff[i]<<" ";
+		fab(0,n,i)
+		{	
+			ll int k;
+			if(i==0)
+			 k=sum-diff[n-1];
+			else
+				k=sum-diff[i-1];
+			ll int temp=a[i]+k;
+			//cout<<"temp"<<k<<" "<<a[i]<<" \n";
+			if(temp<ans)
+				{ans=temp;
+					ind=i;}
+		}
+		/*ans+=a[ind];
+		cout<<ind<<endl;
+		ll int dam=b[ind];
+		ll int i=ind+1;
+		while(i%n!=ind)
+		{
+			if(a[i%n]-dam>0)
+			{
+				
+				ans+=a[i%n]-dam;
+				
+			}
+			dam=b[i%n];
+			i++;
 
-}
-return 0;
+		}*/
+		//cout<<"ind="<<ind<<endl;
+		cout<<ans<<endl;
+		
+	}
+	
+
+	return 0;
 }
