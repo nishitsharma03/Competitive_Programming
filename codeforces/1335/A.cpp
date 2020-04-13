8411d15a6cbae2f1cad5f040eea3cc5e9cb9eaab
Nishit Sharma <nishitsharma0@gmail.com>
2020-04-13 20:11:00 +0530

    Add solution for problem `A - Candies and Two Sisters` Link: https://codeforces.com/contest/1335/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1335/submission/76511399

---
+++ codeforces/1335/A.cpp
@@ -0,0 +1,38 @@
+//#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long 
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll int n;
+		cin>>n;
+		ll int k=n/2;
+		//cout<<"k="<<k<<" ";
+		//if(n%2==0)
+			cout<<n-k-1<<endl;
+		//else
+		//cout<<n-k+1<<endl;
+	}
+	
+
+	return 0;
+}
