e7b44308e7a593d24985d6c4daf6de633d0e51f8
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-16 05:43:00 +0530

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13242013

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -1,6 +1,6 @@
 //#pragma GCC optimize "trapv"
 #include<bits/stdc++.h>
-#define ll long long int
+#define ll long long 
 #define fab(a,b,i) for(int i=a;i<b;i++)
 #define pb push_back
 #define db double
@@ -12,43 +12,57 @@
 #define MOD 1000000007
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-
+vector< vector<int> > dp;
+int n;
+int cnt;
+/*int recurse(int pos,int last,vector< vector<int> > v)
+{
+	//cnt++;
+	//	cout<<cnt<<endl;
+	if(dp[pos][last]!=-1)
+		return dp[pos][last];
+	//v[pos][last]=-1;
+	if(pos==n-1)
+	{
+		v[pos][last]=-1;
+		return dp[pos][last]=*max_element(all(v[pos]));
+	}
+	if(last==0)
+	return dp[pos][last]=max({recurse(pos+1,1,v)+v[pos][1],recurse(pos+1,2,v)+v[pos][2]});
+	else if(last==1)
+		return dp[pos][last]=max({recurse(pos+1,0,v)+v[pos][0],recurse(pos+1,2,v)+v[pos][2]});
+	else
+		return dp[pos][last]=max(recurse(pos+1,1,v)+v[pos][1],recurse(pos+1,0,v)+v[pos][0]);
+}*/
 int main()
 { quick;
 
 
-    {
-        int n;
-        cin>>n;
-        ll mat[n][3];
-        fab(0,n,i)
-        {
-            fab(0,3,j)
-            cin>>mat[i][j];
-        }
-        ll dp[n][3];
-        memset(dp,0,sizeof(dp));
-        fab(0,3,i)
-        dp[0][i]=mat[0][i];
-        fab(1,n,i)
-        {
-            fab(0,3,j)
-            {
-                fab(0,3,k)
-                {
-                    if(k!=j)
-                    dp[i][j]=max(dp[i][j],dp[i-1][k]+mat[i][j]);
-                }
-                
-            }
-            
+	cin>>n;
+	vector< vector<int> > a(n, vector<int>(3));
+	fab(0,n,i)
+	{
+		cin>>a[i][0]>>a[i][1]>>a[i][2];
 
-        }
-        cout<<max({dp[n-1][0],dp[n-1][1],dp[n-1][2]})<<endl;
-    }
-    
-    
+	}
+	dp= vector< vector<int> > (n,vector<int>(3,0));
+	int ans=-1;
+	fab(0,3,i)
+	dp[0][i]=a[0][i];
+	fab(1,n,i)
+	{
+		fab(0,3,j)
+		{
+			fab(0,3,k)
+			if(j!=k)
+			{
+				dp[i][j]=max(dp[i][j],dp[i-1][k]+a[i][j]);
+			}
+		}
+	}
+	cout<<*max_element(all(dp[n-1]))<<endl;
+	//for(auto p:dp[n-1])
+	//	cout<<p<< " ";
 
- cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-    return 0;
+	return 0;
 }
