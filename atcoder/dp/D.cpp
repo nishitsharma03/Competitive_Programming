14e2f929b7d4a1474129acf4d9122c99e2b0a69e
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-16 06:15:00 +0530

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13242298

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -1,6 +1,6 @@
 //#pragma GCC optimize "trapv"
 #include<bits/stdc++.h>
-#define ll long long int
+#define ll long long 
 #define fab(a,b,i) for(int i=a;i<b;i++)
 #define pb push_back
 #define db double
@@ -12,34 +12,41 @@
 #define MOD 1000000007
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-int n,w;
-ll a[105],b[105];
-ll dp[105][100005];
-
-ll recurse(int weight,int ind)
+vector< vector<ll int> > dp;
+ll int a[105],b[105];
+ll int n,w;
+ll int knapsack(int pos,ll int w)
 {
-    if(ind>=n)
-        return 0;
-    if(dp[ind][weight]!=-1)
-        return dp[ind][weight];
-    dp[ind][weight]=max(dp[ind][weight],recurse(weight,ind+1));
-    if(weight+a[ind]<=w)
-        dp[ind][weight]=max(dp[ind][weight],recurse(weight+a[ind],ind+1)+b[ind]);
-    return dp[ind][weight];
+	if(w<=0)
+		return 0;
+	if(pos>=n)
+		return 0;
+
+
+	if(dp[pos][w]!=-1)
+		return dp[pos][w];
+	if(w<a[pos])
+		return dp[pos][w]=knapsack(pos+1,w);	
+	if(pos==n-1)
+		return dp[pos][w]=b[pos]; 
+	return dp[pos][w]=max(knapsack(pos+1,w),knapsack(pos+1,w-a[pos])+b[pos]);
+
+
 
 }
 
 int main()
 { quick;
 
-    memset(dp,-1,sizeof(dp));
-    cin>>n>>w;
-    fab(0,n,i)
-    cin>>a[i]>>b[i];
-    
-    cout<<recurse(0,0)<<endl; 
-    
+	
+	cin>>n>>w;
+	
+	fab(0,n,i)
+	cin>>a[i]>>b[i];
+	dp=vector< vector<ll int> > (n,vector<ll int>(w+1,-1));
+	knapsack(0,w);
+	cout<<dp[0][w]<<endl;
 
- cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-    return 0;
+
+	return 0;
 }
