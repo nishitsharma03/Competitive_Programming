b243cca476d9956a3ccd30d34dfb60df46f169ad
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-18 02:04:00 +0530

    Add solution for problem `E - Knapsack 2` Link: https://atcoder.jp/contests/dp/tasks/dp_e Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13359601

--- atcoder/dp/E.cpp
+++ atcoder/dp/E.cpp
@@ -10,45 +10,44 @@
 #define se second
 #define all(x) x.begin(),x.end()
 #define MOD 1000000007
+#define BIG 1e10
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-const int inf=1e9+5;
-int n,w;
-ll a[105],b[105];
-ll dp[105][100005];
-
-ll recurse(int ind,int val)
+ll dp[100005][105];
+ll a[105],b[105],n,w;
+ll recurse(ll val,int i)
 {
-    if(val<=0)
-        return 0;
-    if(ind>=n)
-        return inf;
-    if(dp[ind][val]!=-1)
-        return dp[ind][val];
-    dp[ind][val]=min(recurse(ind+1,val),recurse(ind+1,val-b[ind])+a[ind]);
-    return dp[ind][val];
-}
+	if(val<=0)
+		return 0;
 
+	if(i==n)
+		return BIG;
+	if(dp[val][i]!=-1)
+		return dp[val][i];
+	dp[val][i]=min(recurse(val,i+1),a[i]+recurse(val-b[i],i+1));
+
+	return dp[val][i];
+		
+
+
+}
 int main()
 { quick;
 
-    memset(dp,-1,sizeof(dp));
-    cin>>n>>w;
-   // cout<<"w:"<<w<<endl;
-    fab(0,n,i)
-    cin>>a[i]>>b[i];
-   
+	//ll n,w;
+	cin>>n>>w;
+	
+	fab(0,n,i)
+	cin>>a[i]>>b[i];
+	memset(dp,-1,sizeof(dp));
+	for(int i=100002;i>=0;i--)
+	{
+			//cout<<recurse(i,0)<<endl;
+			if(recurse(i,0)<=w)
+				{cout<<i;
+			return 0;}
+		
+	}
 
-    for(int i=1e5+2;i>=0;i--)
-    {
-        if(recurse(0,i)<=w)
-        {
-            cout<<i<<endl;
-            return 0;
-        }
-    } 
-    
-
- cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-    return 0;
+	return 0;
 }
