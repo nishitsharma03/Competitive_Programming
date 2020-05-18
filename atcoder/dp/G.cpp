ddcc2ec5695f2d597b24d75c6cba735d56070413
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-19 03:42:00 +0530

    Add solution for problem `G - Longest Path` Link: https://atcoder.jp/contests/dp/tasks/dp_g Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13389344

--- atcoder/dp/G.cpp
+++ atcoder/dp/G.cpp
@@ -12,44 +12,45 @@
 #define MOD 1000000007
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
+int n,m,dp[100005],vis[100005];
 vector<int> v[100005];
-ll dp[100005];
-ll dfs(int src, bool vis[])
+int dfs(int src)
 {
-    vis[src]=1;
-    for(auto i:v[src])
-    {
-        if(!vis[i])
-            dfs(i,vis);
-        dp[src]=max(dp[src],dp[i]+1);
-    }
-    return dp[src];
+	vis[src]=true;
+	for(auto i:v[src])
+	{
+		if(!vis[i])
+			dfs(i);
+		dp[src]=max(dp[src],dp[i]+1);
+	}
+	return dp[src];
 }
+void longpath()
+{
+	int ans=0;
+	fab(1,n+1,i)
+	{
+		if(!vis[i])
+			dfs(i);
+		ans=max(ans,dp[i]);
+	}
+	cout<<ans<<endl;
+}
+
+
+
 int main()
 { quick;
 
-    int n,m;
     cin>>n>>m;
     fab(0,m,i)
     {
-        int a,b;
-        cin>>a>>b;
-        a--;b--;
-        v[a].pb(b);
-       
-    }
-    ll ans=0;
-    bool vis[n];
-    memset(vis,0,sizeof(vis));
-    fab(0,n,i)
-    {
-        if(!vis[i])
-            dfs(i,vis);
-        ans=max(ans,dp[i]);
-    }
-   
-    cout<<ans<<endl;
+    	int a,b;
 
- cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-    return 0;
+    	cin>>a>>b;
+    	//cout<<a<<" "<<b<<endl;
+    	v[a].pb(b);}
+	longpath();
+
+	return 0;
 }
