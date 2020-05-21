5e32be6ad0a11f7fce37f541e3cbb62b35d6d1dc
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-22 02:49:00 +0530

    Add solution for problem `O - Matching` Link: https://atcoder.jp/contests/dp/tasks/dp_o Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13465520

--- atcoder/dp/O.cpp
+++ atcoder/dp/O.cpp
@@ -12,36 +12,33 @@
 #define MOD 1000000007
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-const int N=21;
-ll dp[1LL<<N+5];
+//dp with bitmasking
+ll dp[1<<21+1];
 int main()
 { quick;
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    int n;
-    cin>>n;
-    bool match[n][n];
-    fab(0,n,i)
-    fab(0,n,j)
-    cin>>match[i][j];
-    dp[0]=1;
-    fab(0,1<<n,i)
-    {
-        int setbits=__builtin_popcount(i);
-        fab(0,n,j)
-        {
-            if(((1<<j)&i)==0 and match[setbits][j])
-            {
-                dp[i|(1<<j)]+=dp[i];
-                dp[i|(1<<j)]%=MOD;
-            }
-        }
-    }
-    cout<<dp[(1<<n)-1]<<endl;
-    
 
- cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-    return 0;
+	int n;
+	cin>>n;
+	bool allowed[n][n];
+	fab(0,n,i)
+	fab(0,n,j)
+	cin>>allowed[i][j];
+	
+	dp[0]=1;
+	for(int mask=0;mask<(1<<n);mask++)
+	{
+		int x=__builtin_popcount(mask);
+		for(int i=0;i<n;i++)
+		{
+			if(allowed[x][i] and (mask&(1LL<<i))==0)
+			{
+				dp[mask|(1LL<<i)]=(dp[mask|(1LL<<i)]+dp[mask])%MOD;
+			}
+		}
+	}
+
+	cout<<dp[(1<<n)-1]<<endl;
+
+
+	return 0;
 }
