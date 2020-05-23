e20cd0ee11b386dde0fd0fb15f3b5427bd366f5b
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-24 01:01:00 +0530

    Add solution for problem `P - Independent Set` Link: https://atcoder.jp/contests/dp/tasks/dp_p Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/13552465

--- atcoder/dp/P.cpp
+++ atcoder/dp/P.cpp
@@ -12,41 +12,49 @@
 #define MOD 1000000007
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-const int N=1e5+5;
-vector<int> v[N];
-ll dp[N][2];
-int n;
-void dfs(int src,int par)
-{
-    dp[src][0]=dp[src][1]=1;
-    for(auto i: v[src])
-    {
-        if(i==par)
-            continue;
-        dfs(i,src);
-        dp[src][0]*=dp[i][1];
-        dp[src][0]%=MOD;
-        dp[src][1]=(dp[src][1]*dp[i][0] + dp[src][1]*dp[i][1])%MOD;
-    }
+vector<int> v[100005];
+ll dp[100005][2];
+void dfs(int src,int par )
 
+{
+	//cout<<"src:"<<src<<" "<<pr<<endl;
+	\
+	dp[src][0]=dp[src][1]=1;
+
+	for(auto i: v[src])
+	{
+		if(i==par)
+			continue;
+			dfs(i,src);
+		
+			dp[src][0]=((dp[i][1]+dp[i][0])*dp[src][0])%MOD;
+	
+		
+			dp[src][1]=(dp[src][1]*dp[i][0])%MOD;
+
+	}
+	//cout<<"dp:"<<dp[src][pr]<<endl;
 }
 
 int main()
 { quick;
 
-    cin>>n;
-    fab(0,n-1,i)
-    {
-        int x,y;
-        cin>>x>>y;
-        x--;
-        y--;
-        v[x].pb((y));
-        v[y].pb(x);
-    }
-    dfs(0,-1);
-    cout<<(dp[0][0]+dp[0][1])%MOD;
+	int n;
+	cin>>n;
+	fab(0,n+1,i)
+	dp[i][0]=dp[i][1]=-1;
+	fab(1,n,i)
+	{
+		int x,y;
+		cin>>x>>y;
+		x--;
+		y--;
+		v[x].pb(y);
+		v[y].pb(x);
 
- cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
-    return 0;
+	}
+	dfs(0,-1);
+	cout<<(dp[0][1]+dp[0][0])%MOD<<endl;
+
+	return 0;
 }
