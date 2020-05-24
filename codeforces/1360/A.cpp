0deb4073477f7704bad033edb602ab742a9fa17f
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-24 19:18:00 +0530

    Add solution for problem `A - Minimal Square` Link: https://codeforces.com/contest/1360/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1360/submission/81218149

---
+++ codeforces/1360/A.cpp
@@ -0,0 +1,40 @@
+//#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int a,b;
+		cin>>a>>b;
+		int req=2*min(a,b);
+		if(req<max(a,b))
+		{
+			cout<<max(a,b)*max(a,b)<<endl;
+			continue;
+		}
+		
+		cout<<req*req<<endl;
+	}
+	
+
+	return 0;
+}
