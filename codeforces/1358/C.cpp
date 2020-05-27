c8563796d509fde59d11b8f318d7847929088a9d
Nishit Sharma <nishitsharma0@gmail.com>
2020-05-27 20:12:00 +0530

    Add solution for problem `C - Celex Update` Link: https://codeforces.com/contest/1358/problem/C Tags: math, *1600 Ref: https://codeforces.com/contest/1358/submission/81629623

---
+++ codeforces/1358/C.cpp
@@ -0,0 +1,33 @@
+//#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll x,y,a,b;
+		cin>>x>>y>>a>>b;
+		cout<<(x-a)*(y-b)+1<<endl;
+	}
+	
+
+	return 0;
+}
