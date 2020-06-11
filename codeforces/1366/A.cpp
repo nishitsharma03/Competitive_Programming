db1dea5828d014c76e1fef7061856b2053105470
Nishit Sharma <nishitsharma0@gmail.com>
2020-06-11 20:16:00 +0530

    Add solution for problem `A - Shovels and Swords` Link: https://codeforces.com/contest/1366/problem/A Tags: binary search, greedy, math, *1100 Ref: https://codeforces.com/contest/1366/submission/83394967

---
+++ codeforces/1366/A.cpp
@@ -0,0 +1,40 @@
+//#pragma GCC optimize "trapv"
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int a,b;
+		cin>>a>>b;
+		int ans=0;
+		int make=a/2;
+		if(a==0 or b==0)
+			{cout<<0<<endl;
+		continue;}
+		ans=min(min(a,b),(a+b)/3);
+		cout<<ans<<endl;
+	}
+	
+
+ cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
