0f70755950cc15e6beb7e16a1a1d45df50152037
Nishit Sharma <nishitsharma0@gmail.com>
2020-08-14 20:13:00 +0530

    Add solution for problem `A - Bad Triangle` Link: https://codeforces.com/contest/1398/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1398/submission/89888482

---
+++ codeforces/1398/A.cpp
@@ -0,0 +1,71 @@
+//#pragma GCC optimize "trapv"
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx2")
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("D:/sublime/input.txt", "r", stdin);
+    freopen("D:/sublime/output.txt", "w", stdout);
+#endif
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		ll a[n];
+		fab(0,n,i)
+		cin>>a[i];
+		//reverse(a,a+n);
+		vector<int> v;
+		
+		
+		{
+			if(a[n-1]>=a[0]+a[1])
+			{
+				v.pb(1);
+				v.pb(2);
+				v.pb(n);
+							}
+							
+		}
+		if(v.size()==0)
+			cout<<-1<<endl;
+		else
+		{
+			for(auto i:v)
+				cout<<i<<" ";
+			cout<<endl;
+		}
+
+
+	}
+	
+
+ cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
