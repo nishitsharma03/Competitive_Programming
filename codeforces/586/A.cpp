3e7f2240574cee86c1427460ce054fdc161d30dc
Nishit Sharma <nishitsharma0@gmail.com>
2020-12-05 03:56:00 +0530

    Add solution for problem `A - Alena's Schedule` Link: https://codeforces.com/contest/586/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/586/submission/100406634

---
+++ codeforces/586/A.cpp
@@ -0,0 +1,86 @@
+//#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("D:/sublime/input.txt", "r", stdin);
+    freopen("D:/sublime/output.txt", "w", stdout);
+#endif
+    int n;
+    cin>>n;
+    int a[n];
+    fab(0,n,i)
+    {
+        cin>>a[i];
+    }
+    int st=-1,ed=n-1;
+    fab(0,n,i)
+    {
+        if(a[i])
+        {
+            st=i;
+            break;
+        }
+    }
+    if(st==-1)
+    {
+        cout<<0<<endl;
+        return 0;
+    }
+    for( int i=n-1;i>=0;i--)
+    {
+        if(a[i])
+        {
+            ed=i;
+            break;
+        }
+    }
+    int cnt=ed-st+1;
+
+    fab(st,ed,i)
+    {
+        int tot=0;
+        while(i<=ed and a[i]==0)
+        {
+            i++;
+            tot++;
+        }
+        if(tot>=2)
+        {
+            cnt-=tot;
+        }
+
+
+
+    }
+    cout<<cnt<<endl;
+
+ cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
