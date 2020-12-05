442a2c49d3b0417d0bb12dbc79c385223660d152
Nishit Sharma <nishitsharma0@gmail.com>
2020-12-05 16:36:00 +0530

    Add solution for problem `A - Meeting of Old Friends` Link: https://codeforces.com/contest/714/problem/A Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/714/submission/100448131

---
+++ codeforces/714/A.cpp
@@ -0,0 +1,55 @@
+//#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+
+using namespace std;
+
+int main()
+{ quick;
+#ifndef ONLINE_JUDGE
+    freopen("D:/sublime/input.txt", "r", stdin);
+    freopen("D:/sublime/output.txt", "w", stdout);
+#endif
+    ll l1,l2,r1,r2;
+    cin>>l1>>r1>>l2>>r2;
+    ll k;
+    cin>>k;
+    l1=max(l1,l2);
+    r1=min(r1,r2);
+    if(l1>r1)
+    {
+        cout<<0;
+        return 0;
+    }
+    else
+    {
+        cout<<(r1-l1+1+(r1>=k and k>=l1?-1:0));
+            return 0;
+    }
+    
+
+ cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
