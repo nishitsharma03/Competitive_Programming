ccd5b9c116e32797bd74277fb74c074bc80bbc6b
Nishit Sharma <nishitsharma0@gmail.com>
2020-12-17 22:01:00 +0530

    Add solution for problem `B - Find The Array` Link: https://codeforces.com/contest/1463/problem/B Tags: bitmasks, constructive algorithms, greedy, *1400 Ref: https://codeforces.com/contest/1463/submission/101576540

---
+++ codeforces/1463/B.cpp
@@ -0,0 +1,93 @@
+//#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+
+using namespace std;
+
+int main()
+{	quick;
+#ifndef ONLINE_JUDGE
+	freopen("D:/sublime/input.txt", "r", stdin);
+	freopen("D:/sublime/output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		ll sum = 0;
+		ll a[n];
+		ll mx = -1;
+		fab(0, n, i)
+		{
+			cin >> a[i];
+			sum += a[i];
+		}
+
+
+		bool ok = 0;
+		vector<int> b(n);
+		ll tot = 0;
+		fab(0, n, i)
+		{
+			tot += (a[i] - 1);
+			i++;
+
+		}
+		if (tot * 2LL <= sum)
+		{
+
+			fab(0, n, i)
+			{
+				if (i % 2)
+					cout << a[i] << " ";
+				else
+					cout << 1 << " ";
+
+			}
+
+		}
+		else
+		{
+			fab(0, n, i)
+			{
+				if (i % 2)
+					cout << 1 << " ";
+				else
+					cout << a[i] << " ";
+			}
+
+		}
+		cout << endl;
+
+
+
+	}
+
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
