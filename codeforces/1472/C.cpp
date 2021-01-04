3b0ccf08132c6dcc4a59e49cb9846096339b4cbc
Nishit Sharma <nishitsharma0@gmail.com>
2021-01-04 20:16:00 +0530

    Add solution for problem `C - Long Jumps` Link: https://codeforces.com/contest/1472/problem/C Tags: dp, graphs Ref: https://codeforces.com/contest/1472/submission/103199445

---
+++ codeforces/1472/C.cpp
@@ -0,0 +1,77 @@
+//#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+
+using namespace std;
+const int N = 2e5 + 5;
+ll dp[N];
+int n;
+ll a[N];
+
+ll go(int i)
+{
+	if (i >= n)
+		return 0;
+	if (dp[i] != -1)
+	{
+		return dp[i];
+	}
+	dp[i] = go(i + a[i]) + a[i];
+	return dp[i];
+}
+
+int main()
+{	quick;
+#ifndef ONLINE_JUDGE
+	freopen("D:/sublime/input.txt", "r", stdin);
+	freopen("D:/sublime/output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		cin >> n;
+		fab(0, n, i)
+		{
+			cin >> a[i];
+			dp[i] = -1;
+
+		}
+		ll ans = -1;
+
+		fab(0, n, i)
+		{
+			go(i);
+			ans = max(ans, dp[i]);
+		}
+		cout << ans << endl;
+
+
+	}
+
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
