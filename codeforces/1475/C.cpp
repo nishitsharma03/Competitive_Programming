69678758da77b3593c485251be4e76895ba08082
Nishit Sharma <nishitsharma0@gmail.com>
2021-01-25 20:49:00 +0530

    Add solution for problem `C - Ball in Berland` Link: https://codeforces.com/contest/1475/problem/C Tags: combinatorics, dfs and similar, graphs, hashing, math Ref: https://codeforces.com/contest/1475/submission/105340350

---
+++ codeforces/1475/C.cpp
@@ -0,0 +1,83 @@
+//#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+ll lcm(ll x, ll y) { ll res = x / __gcd(x, y); return (res * y);}
+
+
+int main()
+{	quick;
+#ifndef ONLINE_JUDGE
+	freopen("D:/sublime/input.txt", "r", stdin);
+	freopen("D:/sublime/output.txt", "w", stdout);
+#endif
+	int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		ll boy, girl, k;
+		cin >> boy >> girl >> k;
+
+		set<pair<int, int>> s;
+		map<int, vector<int>  > m1, m2;
+		vector<int> a(k), b(k);
+		fab(0, k, i)
+		{
+			cin >> a[i];
+
+
+		}
+		fab(0, k, i)
+
+		{
+			cin >> b[i];
+			s.insert(mp(max(a[i], b[i]), min(a[i], b[i])));
+			m1[a[i]].pb(b[i]);
+			m2[b[i]].pb(a[i]);
+
+		}
+		ll ans = 0;
+
+		fab(0, k, i)
+		{
+			ll val = k - m1[a[i]].size();
+			val -=  m2[b[i]].size();
+			val++;
+			ans += val;
+
+		}
+		cout << ans / 2 << endl;
+
+
+
+
+	}
+
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
