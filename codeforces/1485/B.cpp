32d8673b7eeeea0efcf1db48b617406ff180695d
Nishit Sharma <nishitsharma0@gmail.com>
2021-02-12 20:52:00 +0530

    Add solution for problem `B - Replace and Keep Sorted` Link: https://codeforces.com/contest/1485/problem/B Tags: dp, implementation, math, *1200 Ref: https://codeforces.com/contest/1485/submission/107203530

---
+++ codeforces/1485/B.cpp
@@ -0,0 +1,93 @@
+//#pragma GCC optimize "trapv"
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+#define ll long long int
+#define fab(a,b,i) for(int i=a;i<b;i++)
+#define pb push_back
+#define db double
+#define mp make_pair
+#define endl "\n"
+#define f first
+#define se second
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
+ll lcm(ll x, ll y) { ll res = x / __gcd(x, y); return (res * y);}
+
+
+int main()
+{	quick;
+#ifndef ONLINE_JUDGE
+	freopen("D:/sublime/input.txt", "r", stdin);
+	freopen("D:/sublime/output.txt", "w", stdout);
+#endif
+	int t = 1;
+	//cin >> t;
+	while (t--)
+	{
+		int n, q;
+		ll k;
+		cin >> n >> q >> k;
+		vector<int> v;
+
+		fab(0, n, i)
+		{
+			int x;
+			cin >> x;
+			v.pb(x);
+		}
+		vector<ll> pref(n);
+		fab(1, n, i)
+		{
+			pref[i] = v[i] - v[i - 1] - 1;
+			pref[i] *= 2;
+
+		}
+
+		// fab(0, n, i)
+		// {
+		// 	cout << pref[i] << " ";
+		// }
+		// cout << endl;
+
+		fab(1, n, i)
+		{
+			pref[i] += pref[i - 1];
+
+
+		}
+
+		while (q--)
+		{
+			int l, r;
+			cin >> l >> r;
+			l--;
+			r--;
+			//cout << "l:" << l << " " << r << endl;
+			ll ans = pref[r] - (l >= 0 ? pref[l] : 0);
+			//cout << "ansl:" << ans << endl;
+			ans += (k - v[r]) + v[l] - 1;
+			cout << ans << endl;
+		}
+
+
+	}
+
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
